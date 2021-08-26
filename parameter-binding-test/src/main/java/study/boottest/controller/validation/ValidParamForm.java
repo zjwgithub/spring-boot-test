46fa957892d62c8a7599710f8eca815cfc6b7916
hothoony <hothoony@hanmail.net>
2021-08-26 10:08:37 +0900

    .

---
+++ parameter-binding-test/src/main/java/study/boottest/controller/validation/ValidParamForm.java
@@ -0,0 +1,17 @@
+package study.boottest.controller.validation;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ValidParamForm {
+    
+    @NotBlank
+    private String username;
+    
+    @NotNull
+    private Integer age;
+}
